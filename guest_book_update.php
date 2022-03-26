e83d19397c658c81ce983524c38f5fefdaa45a8c
MD. WASIFUR RAHMAN SIYAM <71380364+Wasifur-Rahman-Siyam@users.noreply.github.com>
2022-03-27 00:40:27 +0600

    adding edit in an update page

---
+++ guest_book_update.php
@@ -0,0 +1,34 @@
+<?php
+include_once ($_SERVER[ 'DOCUMENT_ROOT']."/Pondit_Practice/OOP/guest-book/config.php");
+
+use App\Utility\Sanitizer;
+use App\Utility\Validator;
+use App\Utility\Utility;
+
+$guest_position=$_GET['guest_position'];
+$guests = [];
+if(array_key_exists('guest_book_data', $_COOKIE)){
+  $guests = unserialize($_COOKIE['guest_book_data']);
+}
+
+if(Utility::is_posted()){
+    $sanitizeData = Sanitizer::sanitize($_POST);
+    $validateData = Validator::validate($sanitizeData);
+
+  if($validateData){
+    $guests [$guest_position] = $validateData;
+    $result= setcookie('guest_book_data', serialize($guests) ,time()+(3600),"/");
+  }
+  else{
+    header('guest_book_from.php');
+  }
+  if($result){
+    echo "Data has been update successfully <a href='guest_book_index.php'> Go to index</a>";
+  }
+  else{
+    echo "Data has not stored";
+  }
+    
+}
+
+?>
